1a6c5550aff01b8e071a9fe513beae0e993e621c
cristipetrusan <george.cristian.petrusan@gmail.com>
2023-01-09 19:03:22 +0200

    added Delete method

--- my-new-app/Controllers/CityDangersController.cs
+++ my-new-app/Controllers/CityDangersController.cs
@@ -61,4 +61,26 @@ public class CityDangersController : ControllerBase
 
         Console.WriteLine(customerEntity);
     }
+
+    [HttpDelete("{partitionKey}/{rowKey}")]
+    public async Task DeleteAsync(string partitionKey, string rowKey)
+    {
+        CloudStorageAccount storageAccount;
+        storageAccount = CloudStorageAccount.Parse(Const.STORAGE_CONNECTION_STRING);
+
+        CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+        CloudTable table = tableClient.GetTableReference(Const.TABLE_NAME);
+
+        // Retrieve the entity to delete.
+        TableOperation retrieveOperation = TableOperation.Retrieve<CityDanger>(partitionKey, rowKey);
+        TableResult result = await table.ExecuteAsync(retrieveOperation);
+        CityDanger entityToDelete = result.Result as CityDanger;
+
+        // Delete the entity.
+        if (entityToDelete != null)
+        {
+            TableOperation deleteOperation = TableOperation.Delete(entityToDelete);
+            await table.ExecuteAsync(deleteOperation);
+        }
+    }
 }
